2a7658dca5be99789c4470b94ff3ad09b69b7848
yavuzlule <30792626+yavuzlule@users.noreply.github.com>
2023-12-11 23:01:28 +0300

    Add files via upload

---
+++ client/client/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox_channels = new ComboBox();
+            button_subscribe = new Button();
+            richTextBox_logs = new RichTextBox();
+            button_send = new Button();
+            textBox_message = new TextBox();
+            textBox_ip = new TextBox();
+            textBox_port = new TextBox();
+            button_connect = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox_username = new TextBox();
+            SuspendLayout();
+            // 
+            // comboBox_channels
+            // 
+            comboBox_channels.Enabled = false;
+            comboBox_channels.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            comboBox_channels.FormattingEnabled = true;
+            comboBox_channels.Items.AddRange(new object[] { "SPS101", "IF100" });
+            comboBox_channels.Location = new Point(34, 208);
+            comboBox_channels.Name = "comboBox_channels";
+            comboBox_channels.Size = new Size(327, 44);
+            comboBox_channels.TabIndex = 0;
+            comboBox_channels.Text = "Select channel...";
+            comboBox_channels.SelectedIndexChanged += comboBox_channels_SelectedIndexChanged;
+            // 
+            // button_subscribe
+            // 
+            button_subscribe.BackColor = Color.LightGreen;
+            button_subscribe.Enabled = false;
+            button_subscribe.FlatStyle = FlatStyle.Flat;
+            button_subscribe.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_subscribe.Location = new Point(386, 208);
+            button_subscribe.Name = "button_subscribe";
+            button_subscribe.Size = new Size(197, 51);
+            button_subscribe.TabIndex = 1;
+            button_subscribe.Text = "Subscribe";
+            button_subscribe.UseVisualStyleBackColor = false;
+            button_subscribe.Click += button_subscribe_Click;
+            // 
+            // richTextBox_logs
+            // 
+            richTextBox_logs.Enabled = false;
+            richTextBox_logs.Location = new Point(34, 297);
+            richTextBox_logs.Name = "richTextBox_logs";
+            richTextBox_logs.Size = new Size(548, 489);
+            richTextBox_logs.TabIndex = 2;
+            richTextBox_logs.Text = "";
+            // 
+            // button_send
+            // 
+            button_send.Enabled = false;
+            button_send.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            button_send.Location = new Point(485, 827);
+            button_send.Margin = new Padding(3, 4, 3, 4);
+            button_send.Name = "button_send";
+            button_send.Size = new Size(105, 44);
+            button_send.TabIndex = 4;
+            button_send.Text = "Send";
+            button_send.UseVisualStyleBackColor = true;
+            button_send.Click += button_send_Click;
+            // 
+            // textBox_message
+            // 
+            textBox_message.Cursor = Cursors.IBeam;
+            textBox_message.Enabled = false;
+            textBox_message.Font = new Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            textBox_message.Location = new Point(34, 827);
+            textBox_message.Margin = new Padding(3, 4, 3, 4);
+            textBox_message.Name = "textBox_message";
+            textBox_message.Size = new Size(443, 39);
+            textBox_message.TabIndex = 5;
+            // 
+            // textBox_ip
+            // 
+            textBox_ip.Location = new Point(34, 129);
+            textBox_ip.Margin = new Padding(3, 4, 3, 4);
+            textBox_ip.Name = "textBox_ip";
+            textBox_ip.Size = new Size(179, 27);
+            textBox_ip.TabIndex = 6;
+            // 
+            // textBox_port
+            // 
+            textBox_port.Location = new Point(233, 129);
+            textBox_port.Margin = new Padding(3, 4, 3, 4);
+            textBox_port.Name = "textBox_port";
+            textBox_port.Size = new Size(220, 27);
+            textBox_port.TabIndex = 7;
+            // 
+            // button_connect
+            // 
+            button_connect.Location = new Point(473, 129);
+            button_connect.Margin = new Padding(3, 4, 3, 4);
+            button_connect.Name = "button_connect";
+            button_connect.Size = new Size(110, 31);
+            button_connect.TabIndex = 8;
+            button_connect.Text = "Connect";
+            button_connect.UseVisualStyleBackColor = true;
+            button_connect.Click += button_connect_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(34, 105);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 20);
+            label1.TabIndex = 9;
+            label1.Text = "IP Address";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(233, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 20);
+            label2.TabIndex = 10;
+            label2.Text = "Port";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(37, 39);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 20);
+            label3.TabIndex = 11;
+            label3.Text = "Username";
+            // 
+            // textBox_username
+            // 
+            textBox_username.Location = new Point(34, 63);
+            textBox_username.Margin = new Padding(3, 4, 3, 4);
+            textBox_username.Name = "textBox_username";
+            textBox_username.Size = new Size(179, 27);
+            textBox_username.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(622, 908);
+            Controls.Add(textBox_username);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button_connect);
+            Controls.Add(textBox_port);
+            Controls.Add(textBox_ip);
+            Controls.Add(textBox_message);
+            Controls.Add(button_send);
+            Controls.Add(richTextBox_logs);
+            Controls.Add(button_subscribe);
+            Controls.Add(comboBox_channels);
+            Name = "Form1";
+            Text = "DiSucord";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox_channels;
+        private Button button_subscribe;
+        private RichTextBox richTextBox_logs;
+        private Button button_send;
+        private TextBox textBox_message;
+        private TextBox textBox_ip;
+        private TextBox textBox_port;
+        private Button button_connect;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox_username;
+    }
+}
---
+++ server/server/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace server
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            richTextBox_events = new RichTextBox();
+            richTextBox_if_subs = new RichTextBox();
+            richTextBox_sps_subs = new RichTextBox();
+            richTextBox_connected_clients = new RichTextBox();
+            button_listen = new Button();
+            textBox_port = new TextBox();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(34, 138);
+            label1.Margin = new Padding(0);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Events";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(469, 138);
+            label2.Name = "label2";
+            label2.Size = new Size(248, 37);
+            label2.TabIndex = 1;
+            label2.Text = "Connected Clients";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(789, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(266, 37);
+            label3.TabIndex = 2;
+            label3.Text = "SPS101 Subscribers";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(1109, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(242, 37);
+            label4.TabIndex = 3;
+            label4.Text = "IF100 Subscribers";
+            // 
+            // richTextBox_events
+            // 
+            richTextBox_events.BackColor = SystemColors.ControlLightLight;
+            richTextBox_events.Location = new Point(34, 179);
+            richTextBox_events.Margin = new Padding(3, 4, 3, 4);
+            richTextBox_events.Name = "richTextBox_events";
+            richTextBox_events.Size = new Size(399, 660);
+            richTextBox_events.TabIndex = 4;
+            richTextBox_events.Text = "";
+            // 
+            // richTextBox_if_subs
+            // 
+            richTextBox_if_subs.Location = new Point(1109, 179);
+            richTextBox_if_subs.Margin = new Padding(3, 4, 3, 4);
+            richTextBox_if_subs.Name = "richTextBox_if_subs";
+            richTextBox_if_subs.Size = new Size(285, 660);
+            richTextBox_if_subs.TabIndex = 5;
+            richTextBox_if_subs.Text = "";
+            // 
+            // richTextBox_sps_subs
+            // 
+            richTextBox_sps_subs.Location = new Point(789, 179);
+            richTextBox_sps_subs.Margin = new Padding(3, 4, 3, 4);
+            richTextBox_sps_subs.Name = "richTextBox_sps_subs";
+            richTextBox_sps_subs.Size = new Size(285, 660);
+            richTextBox_sps_subs.TabIndex = 6;
+            richTextBox_sps_subs.Text = "";
+            // 
+            // richTextBox_connected_clients
+            // 
+            richTextBox_connected_clients.Location = new Point(469, 179);
+            richTextBox_connected_clients.Margin = new Padding(3, 4, 3, 4);
+            richTextBox_connected_clients.Name = "richTextBox_connected_clients";
+            richTextBox_connected_clients.Size = new Size(285, 660);
+            richTextBox_connected_clients.TabIndex = 7;
+            richTextBox_connected_clients.Text = "";
+            // 
+            // button_listen
+            // 
+            button_listen.Location = new Point(339, 66);
+            button_listen.Name = "button_listen";
+            button_listen.Size = new Size(94, 29);
+            button_listen.TabIndex = 8;
+            button_listen.Text = "Listen";
+            button_listen.UseVisualStyleBackColor = true;
+            button_listen.Click += button_listen_Click;
+            // 
+            // textBox_port
+            // 
+            textBox_port.Location = new Point(34, 66);
+            textBox_port.Name = "textBox_port";
+            textBox_port.Size = new Size(299, 27);
+            textBox_port.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(34, 43);
+            label5.Name = "label5";
+            label5.Size = new Size(93, 20);
+            label5.TabIndex = 10;
+            label5.Text = "Port Number";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.GradientActiveCaption;
+            ClientSize = new Size(1445, 908);
+            Controls.Add(label5);
+            Controls.Add(textBox_port);
+            Controls.Add(button_listen);
+            Controls.Add(richTextBox_connected_clients);
+            Controls.Add(richTextBox_sps_subs);
+            Controls.Add(richTextBox_if_subs);
+            Controls.Add(richTextBox_events);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private RichTextBox richTextBox_events;
+        private RichTextBox richTextBox_if_subs;
+        private RichTextBox richTextBox_sps_subs;
+        private RichTextBox richTextBox_connected_clients;
+        private Button button_listen;
+        private TextBox textBox_port;
+        private Label label5;
+    }
+}
